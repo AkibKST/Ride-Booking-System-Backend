1e7873c5be3dd6a8fdafd08518282b565ca2bd16
AkibKST <akibkst22@gmail.com>
2025-12-04 22:36:23 +0600

    cors origin add localhost

--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,7 @@ app.set("trust proxy", 1); // trust first proxy
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: envVars.FRONTEND_URL,
+    origin: [envVars.FRONTEND_URL, "http://localhost:3000"],
     credentials: true,
   })
 );
